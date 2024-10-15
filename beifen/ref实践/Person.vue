77763f58268fa667df527c300d89795bd8879fa8
ksldnasx <wh8261408@126.com>
2024-10-15 15:37:54 +0800

    vue3第一次上手

---
+++ beifen/ref实践/Person.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <h2>地址：{{ a }}</h2>
+        <h3>车名：{{ car.brand }}</h3>
+        <h4>价格：{{ car.price }}万</h4>
+        <button @click="changeName">修改名字</button>
+        <button @click="addage">+1</button>
+        <button @click="cutage">-1</button>
+        <button @click="showTel">查看联系方式</button>
+        <button @click="upperprice">提高价格并改名</button>
+
+        <ul>
+            <!--  :key 中的冒号意思是将key 里面的内容当js代码-->
+            <li @click="upperprice" v-for="g in games" :key="g.id">{{ g.name }}</li>
+        </ul>
+    </div>
+</template>-----------------
+
+<!-- 命名可利用插件简化 -->
+<script lang="ts" setup name="Person">
+
+import { ref } from "vue"
+//数据,
+//ref创建基本类型响应数据对象，利用name.valu改数据
+
+//想要数据是响应式的,要用ref包一下
+let name = ref("zhangs")  //这就是一个实例对象
+let age = ref(18)
+let tel: number | string = "12345643243"
+let a: string = "贵州"
+
+//reactive创建对象类型的响应数据，是深层次的 
+import { reactive } from "vue";
+
+let car = reactive({ brand: "bendci", price: 200 })
+let games = reactive([
+    { id: "iadffasfda1", name: "王者荣耀" },
+    { id: "iadffasfda2", name: "王" },
+    { id: "iadffasfda3", name: "者" }
+])
+//方法
+function upperprice() {
+    car.price += 10
+    for (let i = 0; i <= 10; i++) {
+        games[i].name = "原神"
+    }
+
+}
+//方法
+function changeName() {
+    name.value = "zhang - san"
+}
+function addage() {
+    age.value += 1
+}
+function cutage() {
+    if (age.value < 1) {
+        alert("太小了")
+    } else {
+        age.value -= 1
+    }
+
+}
+function showTel() {
+    alert(tel)
+}
+</script>
+
+<style scoped>
+.person {
+    background: skyblue;
+    box-shadow: 0, 0, 10px;
+    border-radius: 20px;
+    padding: 20px;
+
+}
+
+button {
+    margin: 0 5px;
+    color: #1d35a0;
+    border-radius: 6px
+}
+
+button:hover {
+    background-color: rgb(229, 241, 2);
+    color: red;
+    box-shadow: 0, 0, 3px
+}
+
+;
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class = "person">
+    <!-- br标签是换行 -->
+     <!-- v-model用于数据双向绑定，更改输入框内容则绑定的变量的值也跟着改 -->
+    姓：<input type="text" v-model="firstname"><br>
+    名：<input type="text" v-model="lastname"><br>  
+    全名：<span>{{ fullname }}</span><br> 
+    <!-- 这之后的fullname就再调用计算，因为没变，直接用缓存 -->
+    全名：<span>{{ fullname }}</span><br> 
+    全名：<span>{{ fullname }}</span><br> 
+    全名：<span>{{ fullname }}</span><br> 
+    全名：<span>{{ fullname }}</span><br> 
+    全名：<span>{{ fullname }}</span>
+  </div>
+</template>-----------------
+
+<!-- 命名可利用插件简化 -->
+<script lang="ts" setup name="Person">
+import {computed, ref} from 'vue';
+
+let firstname = ref ("张")
+let lastname = ref("三")
+
+//计算属性
+let fullname = computed(()=>{
+    // console.log("调用一次之后就用不再重复"); 
+    //slice是截取，slice(0,1)是截取字符串第0个字符然后toUpperCase（变大写）再拼接从1开始到结尾的字符（value.slice(1)）
+    return firstname.value.slice(0,1).toUpperCase()+firstname.value.slice(1)+"-"+lastname.value
+})
+</script>
+
+<style scoped>
+.person {
+    background: skyblue;
+    box-shadow: 0, 0, 10px;
+    border-radius: 20px;
+    padding: 20px;
+    width: 440px;
+
+}
+
+button {
+    margin: 0 5px;
+    color: #1d35a0;
+    border-radius: 6px
+}
+
+button:hover {
+    background-color: rgb(229, 241, 2);
+    color: red;
+    box-shadow: 0, 0, 3px
+}
+
+;
+</style>
