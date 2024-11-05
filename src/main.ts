63e6c2d1329f9612d785d00023f24645a7f9e162
ksldnasx <wh8261408@126.com>
2024-11-05 21:46:32 +0800

    import修正

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'  //引入平台创建应用
 import App from './App.vue'      //根组件
 import router from './router'
-import About from './pages/About.vue'
-import Login from './Login.vue'
+
 
 //创建一个应用，以登录页面为初始页面
 const app = createApp(App)    
